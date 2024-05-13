5b9ddc131e8f59884d70be8b578d23bb413218aa
ambhanadi <86625059+ambanadi@users.noreply.github.com>
2024-05-13 07:47:54 -0400

    Add files via upload

---
+++ languages.js
@@ -0,0 +1,14 @@
+const languages = [
+  {
+    no: "0",
+    name: "Auto",
+    native: "Detect",
+    code: "auto",
+  },
+  {
+    no: "1",
+    name: "Myanmar",
+    native: "မြန်မာ",
+    code: "my",
+  },
+];
